ac1f0301d04e67b93f1746bbeeef1753f4675b1e
GY <dev.youngkwok718@gmail.com>
2022-12-10 20:51:51 +0800

    feat render deploy init

--- express.js
+++ express.js
@@ -1,26 +1,21 @@
-import express, { Express, Request, Response } from 'express'
+const express = require('express')
+const playwright = require('./playwright')
+require('dotenv').config()
 
-import dotenv from 'dotenv'
-import playwright from './playwright'
-
-dotenv.config()
-
-const app: Express = express()
+const app = express()
 const port = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.post('/api', async (req: Request, res: Response) => {
-  const response: any = await playwright(req.body)
-
+app.post('/api', async (req, res) => {
+  const response = await playwright(req.body)
   if (response.status === 400) {
     res.status(400).send(response)
   } else {
     res.send(response)
   }
 })
-
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
 })
