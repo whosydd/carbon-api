1e7403a25a4453d2abc8e25362b891ba0d6629a3
GY <dev.youngkwok718@gmail.com>
2023-03-07 13:56:51 +0800

    test: update

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-import playwright from '../utils/playwright'
-
-module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  const response: any = await playwright(req.body)
-  res.status(200).send(response)
-}
